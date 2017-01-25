8e0d9a1ae83d3d5ba3a4769e638ac5926ff9f04b
Laurens Offereins <lmoffereins@gmail.com>
2017-01-25 23:17:13 +0100

    BuddyPress: move members dir search box to the page's side

--- inc/buddypress.php
+++ inc/buddypress.php
@@ -46,21 +46,50 @@ function paco2017_bp_profile_group_has_fields() {
 /**
  * Add markup to the page content header
  *
- * Puts the member header in the page content header.
+ * Puts the member header and other BuddyPress top content
+ * in the page content header.
  *
  * @since 1.0.0
  */
 function paco2017_bp_page_content_header() {
 
-	// Single user
+	// Start output buffer
+	ob_start();
+
+	// Single user item header
 	if ( bp_is_user() ) : ?>
 
-	<div id="buddypress"><!-- Cheat the system: another #buddypress! :o -->
 		<div id="item-header" role="complementary">
 
 			<?php bp_get_template_part( 'members/single/member-header' ); ?>
 
 		</div><!-- #item-header -->
+
+	<?php endif;
+
+	// Directory search
+	if ( bp_is_members_directory() ) : ?>
+
+		<?php /* Backward compatibility for inline search form. Use template part instead. */ ?>
+		<?php if ( has_filter( 'bp_directory_members_search_form' ) ) : ?>
+
+			<div id="members-dir-search" class="dir-search" role="search">
+				<?php bp_directory_members_search_form(); ?>
+			</div><!-- #members-dir-search -->
+
+		<?php else: ?>
+
+			<?php bp_get_template_part( 'common/search/dir-search-form' ); ?>
+
+		<?php endif; ?>
+
+	<?php endif;
+
+	// Wrap logic in #buddypress to force use the styling logic
+	if ( $content = ob_get_clean() ) : ?>
+
+	<div id="buddypress"><!-- Cheat the system: another #buddypress! :o -->
+		<?php echo $content; ?>
 	</div>
 
 	<?php endif;
@@ -79,3 +108,26 @@ function paco2017_bp_page_entry_title() { ?>
 	<?php
 }
 add_action( 'bp_before_member_header_meta', 'paco2017_bp_page_entry_title', 5 );
+
+/**
+ * Act before the members directory page is rendered
+ *
+ * @since 1.0.0
+ */
+function paco2017_bp_before_directory_members() {
+
+	/**
+	 * Prevent members search form from outputting, because we've put it in the
+	 * page content header location. Assume that after this point no search
+	 * form is requested anymore.
+	 */
+	remove_all_filters( 'bp_directory_members_search_form' );
+	add_filter( 'bp_get_template_part', function( $templates, $slug, $name ) {
+		if ( 'common/search/dir-search-form' === $slug ) {
+			$templates = array();
+		}
+
+		return $templates;
+	}, 10, 3 );
+}
+add_filter( 'bp_before_directory_members', 'paco2017_bp_before_directory_members' );
